a1d44952d889c00fab3d197e963f34f0f4b8c7b9
hieu22012003 <101488865+hieu22012003@users.noreply.github.com>
2022-11-07 10:12:17 +0700

    uhki

---
+++ chuong13/sumArray/src/main/java/library/sumarray/SumArray.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package library.sumarray;
+
+/**
+ *
+ * @author Admin
+ */
+public class SumArray {
+
+ public static void main( String args[] )
+     {
+        int array[] = { 87, 68, 94, 100, 83, 78, 85, 91, 76, 87 };
+       int total = 0;
+  
+        
+        for ( int counter = 0; counter < array.length; counter++ )
+           total += array[ counter ];
+  
+        System.out.printf( "Total of array elements: %d\n", total );
+     }   
+  } 
